d570ec3d82e44d8e1ade60371fbfb76c0560391e
vtu25356 <vtu25356@veltech.edu.in>
2026-04-24 14:50:38 +0530

    Add files via upload
    uploaded week6 folder

---
+++ week6/Task6.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> result = new ArrayList<>();
+        
+        if (s.length() < p.length()) return result;
+
+        int[] pCount = new int[26];
+        int[] sCount = new int[26];
+
+        for (char c : p.toCharArray()) {
+            pCount[c - 'a']++;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            sCount[s.charAt(i) - 'a']++;
+
+            if (i >= p.length()) {
+                sCount[s.charAt(i - p.length()) - 'a']--;
+            }
+
+            if (Arrays.equals(pCount, sCount)) {
+                result.add(i - p.length() + 1);
+            }
+        }
+
+        return result;
+    }
+}
